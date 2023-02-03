bb1ec1c40786c6171db81dee954c9160bae5478c
keerthisaladi11 <keerthisaladi12@gmail.com>
2023-02-03 20:59:31 +0530

    jfukj

---
+++ img tag1.java
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> HOME </title>
+</head>
+<body>
+<div style="color:red">
+<h1> hi</h1>
+<h2> hi</h2>
+<h3> hi</h3>
+<h4> hi</h4>
+<h5> hi</h5>
+<h6> hi</h6>
+<p > hi this is first object</P>
+<br>
+<strong> hi this is first object</strong>
+<br>
+<em> hi this is first object</em>
+<br>
+<b> hi</b>
+<br>
+<i> hi</i>
+ <P>hi this is first object hi this is first object</p>
+<br>
+</div>
+<hr>
+<P>hi this is first object hi this is first object</p>
+<ul> HOBBIES </ul>
+	<li>coding</li>
+	<li>watching tv</li>
+	<li>listening music</li>
+ </ul>
+<ol> MAKING TEA
+	<li>go to kitchen </li>
+	<li> a</li>
+	<li>b </li>
+	<li>c </li>
+	<li> d</li>
+</ol>
+<img src="https://th.bing.com/th/id/R.db197cadf38d4731649560829d74ec6f?rik=DtbsnmFTCVXF%2fA&riu=http%3a%2f%2fnews.uoguelph.ca%2fwp-content%2fuploads%2f2016%2f11%2fcat-e1478099247797.jpg&ehk=%2fvNUN2OBPxT57U6L9lyg2msIa4HItQwIEW%2btGQKw2Zs%3d&risl=&pid=ImgRaw&r=0">
+
+</body>
+</html>
